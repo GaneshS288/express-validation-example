efe4f5fb89560d7de07158d8b05ac83cb3eca3dd
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2025-06-29 14:50:48 +0530

    refactor: use validationresult.data instead of req.body for saving to database

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 import express from "express";
 import UserSignupSchema from "./zodUserSchema.js";
-import {z } from "zod/v4"
+import { z } from "zod/v4";
 
 const app = express();
-/**
- * @type {Array<{name: string, password: string}>}
- */
+/** @type {Array<{name: string, password: string}>}*/
 const db = [{ name: "ganesh", password: "Iamevil" }];
 
 app.use(express.json());
@@ -13,14 +11,13 @@ app.use(express.json());
 app.post("/zod/user", async (req, res) => {
     const validationResult = UserSignupSchema.safeParse(req.body);
 
-    if(validationResult.success === false) {
-        const error = z.treeifyError(validationResult.error)
+    if (validationResult.success === false) {
+        const error = z.treeifyError(validationResult.error);
         res.status(400).json(error);
-    }
-    else {
-        db.push(req.body);
+    } else {
+        db.push(validationResult.data);
         res.status(201).json(db);
     }
 });
 
-app.listen(3000, () => console.log("server has started at port 3000"))
+app.listen(3000, () => console.log("server has started at port 3000"));
